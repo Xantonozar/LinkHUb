6c493c77bf53c2999b988ff71f9649fd653cd93c
Ozar <87942063+Xantonozar@users.noreply.github.com>
2025-08-09 15:11:28 +0600

    Update page.jsx

--- app/semester/[semesterId]/page.jsx
+++ app/semester/[semesterId]/page.jsx
@@ -26,7 +26,7 @@ id: 6, title: 'Syllabus Tracker', type: 'Application', url:'https://adib3111.git
     icon: '📚',
     description: 'Core fundamental studies in engineering principles',
     resources: [
-    
+    { id: 1, title: 'Pratik Vai er Note', type: 'Course Materials', url: 'https://drive.google.com/drive/folders/16r6aY18mUBpXSzLFcTkyXHezDhDuPyQK?usp=drive_link' },
     ]
   },
   '2-1': {
